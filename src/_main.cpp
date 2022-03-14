6175d32ddd39485b9e40406439a6a1eb26563de0
Batterie <matteus.santostb@gmail.com>
2022-03-14 17:25:05 -0300

    implemented printing via operator overloading and appending (inserting in sequential list)

--- src/_main.cpp
+++ src/_main.cpp
@@ -1,8 +1,29 @@
-#include <stdio.h>
+#include <iostream>
+#include "./Sequential/List.hpp"
+
+using std::cout;
+using std::endl;
+using namespace DataStructure::Query::Sequential;
+
+void terminateApplication();
 
 int main()
 {
-    printf("Hello world!");
+    List<int> sequentialList = List<int>();
+    cout << sequentialList << endl;
 
+    sequentialList.append(1);
+    cout << sequentialList << endl;
+
+    sequentialList.append(10);
+    cout << sequentialList << endl;
+
+    terminateApplication();
     return 0;
+}
+
+
+void terminateApplication()
+{
+    cout << endl << endl<< endl << "Application terminated." << endl << endl << endl;
 }
