6b85371d13692f54899b30f512a9eda63f10f10d
Batterie <matteus.santostb@gmail.com>
2022-04-24 04:57:06 -0300

    Implementation of shell sort

--- src/Classes/Sequential/List.hpp
+++ src/Classes/Sequential/List.hpp
@@ -12,26 +12,29 @@ namespace DataStructure::Query::Sequential
     class List
     {
     public:
-        T* _first;
+        T *_first;
+
     private:
         unsigned long _size;
 
     public:
-        List(const List<T>& copy) {
+        List(const List<T> &copy)
+        {
             _first = new T[copy.size()];
             std::copy(copy._first, copy._first + copy.size(), _first);
             _size = copy.size();
         }
-        List() {
+        List()
+        {
             this->_first = new T[0];
             this->_size = 0;
         }
-        ~List() {
+        ~List()
+        {
             delete[] this->_first;
         }
 
         unsigned long size() const { return this->_size; }
-
 
         void append(T data);
         void insert(T data, int position);
@@ -39,33 +42,34 @@ namespace DataStructure::Query::Sequential
         void pop();
         void remove(int position);
 
-        T& find(const T data) const;
+        T &find(const T data) const;
         void findAt(int position) const;
 
-        List<T>* selectionSort();
-        List<T>* insertionSort();
-        List<T>* shellSort();
-        List<T>* quickSort();
+        List<T> *selectionSort();
+        List<T> *insertionSort();
+        List<T> *shellSort();
+        List<T> *quickSort();
 
         // temporary solution
-        void print() {
+        void print()
+        {
             for (int index = 0; index < _size; index++)
                 std::cout << _first[index] << '\n';
         }
 
         template <class E>
-        friend ostream& operator<<(ostream& os, const List<E>& list);
-        List<T>& operator=(const List<T>& source);
+        friend ostream &operator<<(ostream &os, const List<E> &list);
+        List<T> &operator=(const List<T> &source);
     };
 
     template <class T>
     void List<T>::append(T data)
     {
-        T* newList = new T[++_size];
+        T *newList = new T[++_size];
 
         for (unsigned int index = 0;
-            index < _size - 1;
-            index++)
+             index < _size - 1;
+             index++)
         {
             newList[index] = _first[index];
         }
@@ -78,12 +82,13 @@ namespace DataStructure::Query::Sequential
     template <class T>
     void List<T>::insert(T data, int position)
     {
-        if (position > _size || position < 0) {
+        if (position > _size || position < 0)
+        {
             std::cout << "Invalid range for insertion.\n";
             return;
         }
 
-        T* newList = new T[++_size];
+        T *newList = new T[++_size];
 
         for (int index = 0; index < position; index++)
             newList[index] = _first[index];
@@ -100,15 +105,16 @@ namespace DataStructure::Query::Sequential
     template <class T>
     void List<T>::pop()
     {
-        if (_size == 0) {
+        if (_size == 0)
+        {
             std::cout << "There are no elements to be removed.\n";
             return;
         }
 
-        T* newList = new T[--_size];
+        T *newList = new T[--_size];
         for (int index = 0;
-            index < _size;
-            index++)
+             index < _size;
+             index++)
         {
             newList[index] = _first[index];
         }
@@ -120,17 +126,19 @@ namespace DataStructure::Query::Sequential
     template <class T>
     void List<T>::remove(int position)
     {
-        if (position > _size || position < 0) {
+        if (position > _size || position < 0)
+        {
             std::cout << "Invalid range for remotion.\n";
             return;
         }
 
-        if (_size == 0) {
+        if (_size == 0)
+        {
             std::cout << "There are no elements to be removed.\n";
             return;
         }
 
-        T* newList = new T[--_size];
+        T *newList = new T[--_size];
         for (int index = 0; index < position; index++)
             newList[index] = _first[index];
 
@@ -142,22 +150,23 @@ namespace DataStructure::Query::Sequential
     }
 
     template <class T>
-    T& List<T>::find(const T data) const
+    T &List<T>::find(const T data) const
     {
         for (int index = 0; index < _size; index++)
-            if (_first[index] == data) return _first[index];
+            if (_first[index] == data)
+                return _first[index];
     }
 
     template <class T>
-    List<T>* List<T>::selectionSort()
+    List<T> *List<T>::selectionSort()
     {
-        for (int currentPosition = 0; currentPosition < _size; currentPosition++)
+        for (long currentPosition = 0; currentPosition < _size; currentPosition++)
         {
             T currentElement = _first[currentPosition];
 
-            int smallestPosition = currentPosition;
+            long smallestPosition = currentPosition;
             T smallestElement = _first[smallestPosition];
-            for (int j = currentPosition + 1; j < _size; j++)
+            for (long j = currentPosition + 1; j < _size; j++)
                 if (_first[j] < smallestElement)
                     smallestElement = _first[smallestPosition = j];
 
@@ -168,14 +177,14 @@ namespace DataStructure::Query::Sequential
         return this;
     }
     template <class T>
-    List<T>* List<T>::insertionSort()
+    List<T> *List<T>::insertionSort()
     {
-        for (int currentPosition = 1; currentPosition < _size; currentPosition++)
+        for (long currentPosition = 1; currentPosition < _size; currentPosition++)
         {
             T currentElement = _first[currentPosition];
-            int replacePosition;
+            long replacePosition;
 
-            for (int j = currentPosition - 1; j >= 0 && currentElement <= _first[j]; j--)
+            for (long j = currentPosition - 1; j >= 0 && currentElement <= _first[j]; j--)
                 _first[replacePosition = j + 1] = _first[j];
 
             _first[replacePosition] = currentElement;
@@ -184,75 +193,83 @@ namespace DataStructure::Query::Sequential
         return this;
     }
     template <class T>
-    List<T>* List<T>::shellSort()
+    List<T> *List<T>::shellSort()
     {
         // example
         //  0  1  2  3  4  5  6  7  8  9
         // 45 23 53 43 18 24 08 95 10 15
+        // gap == 5
+        // 45             24                // sublist 1
+        //    23             08             // sublist 2
+        //       53             95          // sublist 3
+        //          43             10       // sublist 4
+        //             18             15    // sublist 5
 
-        // 45             24
-        //    23             08
-        //       53             95
-        //          43             10
-        //             18             15
-                    
         // 24 08 53 10 15 45 23 95 43 18
-            
-        // 24	 53    15    23    43 
-        //    08    10    45    95    18
-        
+        // gap == 2
+        // 24	 53    15    23    43       // sublist 1
+        //    08    10    45    95    18    // sublist 2
+
         // 15 08 23 10 24 18 53 45 43 95
+        // gap == 1
         // 08 15 23 10 24 18 53 45 43 95
         // 08 10 15 23 24 18 53 45 43 95
         // 08 10 15 18 23 24 53 45 43 95
         // 08 10 15 18 23 24 45 53 43 95
-        // 08 10 15 18 23 24 43 45 53 95  
+        // 08 10 15 18 23 24 43 45 53 95
 
-        int initialGap = _size/2;
-        for (int gap = initialGap; gap > 0; gap /= 2)
+        long initialGap = _size / 2;
+        for (long gap = initialGap; gap > 0; gap /= 2)
         {
-            for (int subListFirst = gap; subListFirst < _size; subListFirst++)
+            for (long subListFirst = gap; subListFirst < _size; subListFirst++)
             {
+                T currentItem = _first[i];
 
+                long replacePosition;
+                for (replacePosition = subListFirst; replacePosition >= gap && _first[replacePosition - gap] > currentItem; replacePosition -= gap)
+                    _first[replacePosition] = _first[replacePosition - gap];
+
+                _first[replacePosition] = currentItem;
             }
         }
 
 
 
-        for(int gap = _size/2; gap > 0; gap /= 2)
-            for(int i = gap; i < _size. i++)
+        for (int gap = _size / 2; gap > 0; gap /= 2)
+            for (int i = gap; i < _size.i++)
             {
                 T temp = _first[i];
 
                 int j;
-                for(j = i; j >= gap && _first[j - gap] > temp; j -= gap)
+                for (j = i; j >= gap && _first[j - gap] > temp; j -= gap)
                     _first[j] = _first[j - gap];
-                
+
                 _first[j] = temp;
             }
     }
 
-
     template <class T>
-    ostream& operator<<(ostream& os, const List<T>& list)
+    ostream &operator<<(ostream &os, const List<T> &list)
     {
-        if (list.size() < 1) return os << "Empty list.\n";
+        if (list.size() < 1)
+            return os << "Empty list.\n";
 
         os << "[" << list._first[0] << "]";
 
-        T* iterator = list._first;
+        T *iterator = list._first;
         for (unsigned int counter = 1;
-            counter < list.size();
-            counter++)
+             counter < list.size();
+             counter++)
             os << "\n[" << iterator[counter] << "]";
 
         return os;
     }
 
     template <class T>
-    List<T>& List<T>::operator=(const List<T>& source)
+    List<T> &List<T>::operator=(const List<T> &source)
     {
-        if (this == &source) return *this;
+        if (this == &source)
+            return *this;
 
         delete[] _first;
 
